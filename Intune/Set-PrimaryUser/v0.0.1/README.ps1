2a8a6ec55d1f591aa976aecaf2ac87e5a7c7270f
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-02-26 16:55:10 -0500

    Update README.ps1

--- Intune/Set-PrimaryUser/v0.0.1/README.ps1
+++ Intune/Set-PrimaryUser/v0.0.1/README.ps1
@@ -56,26 +56,6 @@ $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
 
 $tenant = $userUpn.Host
 
-Write-Host "Checking for AzureAD module..."
-
-    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
-
-    if ($AadModule -eq $null) {
-
-        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
-        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
-
-    }
-
-    if ($AadModule -eq $null) {
-        write-host
-        write-host "AzureAD Powershell module not installed..." -f Red
-        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
-        write-host "Script can't continue..." -f Red
-        write-host
-        exit
-    }
-
 # Getting path to ActiveDirectory Assemblies
 # If the module count is greater than 1 find the latest version
 

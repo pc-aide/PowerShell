28c7ba2145b6defae6542de6694c77b0986ac913
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-17 08:57:31 -0400

    Create

---
+++ Functions/SCCM/get-CollectionName-cmDevice.ps1
@@ -0,0 +1,16 @@
+# CollectionName for cmDevice 
+
+Function sms_CollectionName_cmDevice { 
+
+  # Switch Hostname 
+  param([parameter(Mandatory=$true)] [String]$HostName) 
+
+  (Get-WmiObject -ComputerName MMTRLPALPINF151.Prod.MJQ.Local  -Namespace root/SMS/site_mjq ` 
+
+  -Query "SELECT SMS_Collection.* FROM SMS_FullCollectionMembership, 
+
+  SMS_Collection where name = '$HostName' 
+
+  and SMS_FullCollectionMembership.CollectionID = SMS_Collection.CollectionID").Name 
+
+} 

6195fda52ab9fa75b7955541a14a40100d83a7d6
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-17 09:11:19 -0400

    Update Get-CollectionName-cmDevice.ps1

--- Functions/SCCM/Get-CollectionName-cmDevice.ps1
+++ Functions/SCCM/Get-CollectionName-cmDevice.ps1
@@ -1,17 +1,14 @@
-# CollectionName for cmDevice 
-
-Function Get-CollectionName-cmDevice { 
+Function get-CollectionName-cmDevice { 
 
   # Switch Hostname 
   param([parameter(Mandatory=$true)] [String]$HostName) 
+  # ComputerName = FQDN of our sccM
+  $ComputerName = "SCCM01.local"
+  # Site = 3x characteres of our site
+  $Site = "PRD"
 
-  # ComputerName : <FQDN_SCCM> & root/SMS/site_<3_charactes_of_SCCM>
-  (Get-WmiObject -ComputerName <FQDN_SCCM>  -Namespace root/SMS/site_<3_charactes_of_SCCM> ` 
-
-  -Query "SELECT SMS_Collection.* FROM SMS_FullCollectionMembership, 
-
-  SMS_Collection where name = '$HostName' 
-
-  and SMS_FullCollectionMembership.CollectionID = SMS_Collection.CollectionID").Name 
-
+(Get-WmiObject -ComputerName $ComputerName  -Namespace root/SMS/site_$Site `
+    -Query "SELECT SMS_Collection.* FROM SMS_FullCollectionMembership,
+    SMS_Collection where name = '$HostName' 
+    and SMS_FullCollectionMembership.CollectionID = SMS_Collection.CollectionID").Name 
 } 

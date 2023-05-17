93b3fc4fd9d528fc11143bee0c631ccc6dabb295
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-05-17 11:01:16 -0400

    Update and rename draft_script.ps1 to script.ps1

---
+++ DNS/Function/Get-RecordsDNS/v0.1/script.ps1
@@ -0,0 +1,56 @@
+function Get-RecordsDNS {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Hostname
+    )
+
+    $DomainName = "mtl.local"
+    $ServerName = "DNS"
+    $Output = @{}
+
+    try {
+        $ARecord = Get-DnsServerResourceRecord -ZoneName $DomainName -ComputerName $ServerName -Name $Hostname -ErrorAction Stop |
+                    Select-Object hostname, recordType -ExpandProperty recordData
+
+        $IPv4Address = ($ARecord | Where-Object { $_.recordType -eq 'A' }).IPv4Address
+
+        $Output += @{
+            ARecord = @{
+                Hostname = $ARecord.hostname
+                RecordType = $ARecord.recordType
+                IPv4Address = $IPv4Address
+            }
+        }
+
+        if ($Output.ARecord) {
+            $Output.ARecord
+            Write-Output "`n" # Add an empty line
+        }
+    }
+    catch {
+        Write-Output "Failed to get A record. Checking for PTR record."
+        $ReverseIP = ($Hostname -split '\.')[-1..-4] -join '.'
+        $PTRZoneName = "10.in-addr.arpa"
+
+        $PTRRecord = Get-DnsServerResourceRecord -ZoneName $PTRZoneName -ComputerName $ServerName -RRType Ptr |
+                     Where-Object { $_.RecordData.PtrDomainName -like "*$Hostname*" } |
+                     Select-Object hostname, RecordType -ExpandProperty RecordData
+
+        if ($PTRRecord) {
+            $Output += @{
+                PTRRecord = @{
+                    Hostname = $PTRRecord.hostname
+                    RecordType = $PTRRecord.RecordType
+                    PtrDomainName = $PTRRecord.PtrDomainName
+                }
+            }
+
+            if ($Output.PTRRecord) {
+                $Output.PTRRecord
+            }
+        }
+        else {
+            Write-Output "No PTR record found."
+        }
+    }
+}

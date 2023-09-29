dadf17539b2ce8f8caefe0afbc8b56d3b9b79bee
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-09-29 05:51:51 -0400

    Create Test-PendingReboot.ps1

---
+++ Functions/Test-PendingReboot.ps1
@@ -0,0 +1,26 @@
+<#
+-------------------------------------------------------------------------------------------------------
+    SRC: https://ilovepowershell.com/2015/09/10/how-to-check-if-a-server-needs-a-reboot/
+    Adapted from https://gist.github.com/altrive/5329377
+    Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
+    
+    Fonction -> Boolean
+-------------------------------------------------------------------------------------------------------
+#>
+ 
+#https://imgur.com/JFKZnzN
+function Test-PendingReboot
+{
+ if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
+ if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
+ if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+ try {
+   $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+   $status = $util.DetermineIfRebootPending()
+   if(($status -ne $null) -and $status.RebootPending){
+     return $true
+   }
+ }catch{}
+ 
+ return $false
+}
